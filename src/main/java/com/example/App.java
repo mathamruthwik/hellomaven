6cf522d299b9e70821d07c86851131d734cc7887
mathamruthwik <mathamruthwik26@gmail.com>
2026-08-17 11:08:28 +0530

    app second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! CMRCET" );
+        System.out.println( "Hello World! WELCOME TO CMRCET" );
     }
 }
